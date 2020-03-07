12d6c494729dbaccd2763ab3b056365dfc67bd86
Marcos <marcosav2010@gmail.com>
2020-03-07 19:24:54 +0100

    Cambios en tests

--- src/containers/Feed/feed.test.js
+++ src/containers/Feed/feed.test.js
@@ -3,18 +3,11 @@ import { render, cleanup } from 'react-testing-library';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { FeedContainer } from './feed.container';
 
-const props = {
-  webId: 'https://exmaple.com/#me',
-  image: 'test.png',
-  updatePhoto: 'updated.png',
-  name: 'example'
-};
-
 describe.only('Feed', () => {
   afterAll(cleanup);
   const { container, getByTestId } = render(
     <Router>
-      <FeedContainer {...{ ...props }} />
+      <FeedContainer />
     </Router>
   );
 });

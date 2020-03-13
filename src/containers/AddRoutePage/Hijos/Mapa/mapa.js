37cb8d7f774c357ad09d81779f40e82585e714f2
Luis <uo264691@uniovi.es>
2020-03-13 17:23:45 +0100

    Recogedor de coordenadas

--- src/containers/AddRoutePage/Hijos/Mapa/mapa.js
+++ src/containers/AddRoutePage/Hijos/Mapa/mapa.js
@@ -2,17 +2,26 @@ import React from 'react';
 
 // import { NavBar } from "@components";
 import {
-  withScriptjs, withGoogleMap, GoogleMap
+  withScriptjs, withGoogleMap, GoogleMap, Marker
 } from 'react-google-maps'
 
+
 const Mapa = (props) => {
+
+  function handleClick(event) {
+    var lat1 = event.latLng.lat(), lng1 = event.latLng.lng()
+    console.log(lat1+" "+lng1);
     
+    
+  }
     return (
       <GoogleMap
         defaultZoom={7}
         defaultCenter={{ lat: -34.397, lng: 150.644 }}
         options={{ streetViewControl: false }}
-        mapTypeId={'terrain'}>
+        mapTypeId={'terrain'}
+        onClick={(e) => handleClick(e)}>
+         
       </GoogleMap>
     )
   };
--- src/containers/AddRoutePage/add-route-page.component.js
+++ src/containers/AddRoutePage/add-route-page.component.js
@@ -5,6 +5,7 @@ import Mapa from './Hijos/Mapa/mapa'
 
 
 
+
 const AddRoutePage = () => {
   const googleMapURL = `https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}&v=3.exp&libraries=geometry,drawing,places`
 

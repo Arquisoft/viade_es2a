a9d530e64572039d8f21246c3d91c9729e914334
Luis <uo264691@uniovi.es>
2020-03-13 22:50:12 +0100

    Recoge coordenadas en addroutes

--- src/containers/AddRoutePage/Hijos/Mapa/mapa.js
+++ src/containers/AddRoutePage/Hijos/Mapa/mapa.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 // import { NavBar } from "@components";
 import {
@@ -8,21 +8,48 @@ import {
 
 const Mapa = (props) => {
 
+  
+  const points=[{ lat: -34.397, lng: 150.644 },{ lat: -35.397, lng: 150.644 }];
+/*    {points.map(point => {
+        return(
+         < Marker 
+          position={point} 
+          onMouseOver={() => setVisible(!visible)}
+          onMouseOut={() => setVisible(!visible)}
+          onClick={() => setSelected(!selected)}
+      />
+      
+      )})}*/
+  const [visible, setVisible] = useState(false);
+  const [selected, setSelected] = useState(false);
+
   function handleClick(event) {
     var lat1 = event.latLng.lat(), lng1 = event.latLng.lng()
-    console.log(lat1+" "+lng1);
+
+    points.push({lat: lat1,lng: lng1});
+    console.log(points[points.length-1].lat+" y "+points[points.length-1].lng);
+   
+    {points.map(point => {
+      return(
+        
+       < Marker 
+        position={point}
+        onMouseOver={() => setVisible(!visible)}
+        onMouseOut={() => setVisible(!visible)}
+        onClick={() => setSelected(!selected)}
+    />
     
-    
+    )})}
   }
+
     return (
       <GoogleMap
         defaultZoom={7}
-        defaultCenter={{ lat: -34.397, lng: 150.644 }}
+        defaultCenter={{ lat: 43.355157, lng: -5.851254 }}
         options={{ streetViewControl: false }}
         mapTypeId={'terrain'}
-        onClick={(e) => handleClick(e)}>
-         
-      </GoogleMap>
+        onClick={(e) => handleClick(e)}/>
+   
     )
   };
   
--- src/containers/AddRoutePage/add-route-page.component.js
+++ src/containers/AddRoutePage/add-route-page.component.js
@@ -8,11 +8,11 @@ import Mapa from './Hijos/Mapa/mapa'
 
 const AddRoutePage = () => {
   const googleMapURL = `https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}&v=3.exp&libraries=geometry,drawing,places`
-
+ //const puntos=[{ lat: -34.397, lng: 150.644 },{ lat: -35.397, lng: 150.644 }];
 return (
   <AddRouteHolder>
     <CamposRuta/>
-    <Mapa 
+    <Mapa //{... {puntos}}
         googleMapURL={googleMapURL}
         loadingElement={<p>Cargando</p>}
         containerElement={<MapHolder />}

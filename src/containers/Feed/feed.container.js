4825af7ef0e1e4f335e439e5e9cc17938b58c842
uo265363 <UO265363@uniovi.es>
2020-03-14 18:02:55 +0100

    Feed funcional

--- src/containers/Feed/feed.container.js
+++ src/containers/Feed/feed.container.js
@@ -1,41 +1,58 @@
-import React from 'react';
+import React,{useEffect,useState} from 'react';
 import { RouteMapPageContent } from '../../components/RouteMap/route-map.component';
+import { foaf } from 'rdf-namespaces';
+import { fetchDocument } from 'tripledoc';
+import { storageHelper } from '@utils';
 
 /**
  * Container component for the Feed Page, fetches routes from a POD
  */
 export const FeedContainer = props => {
-  const routes = [
-    {
-      id: "16c67714-f386-4832-93da-5fb7b8ffce13",
-      name: "Ruta 1",
-      author: "patata",
-      date: Date.now(),
-      points: [
-        { lat: -34.397, lng: 150.644 },
-        { lat: -35.297, lng: 149.644 },
-        { lat: -34.297, lng: 148.644 },
-        { lat: -33.397, lng: 147.644 },
-        { lat: -34.197, lng: 146.644 }
-      ]
-    },
-    {
-      id: "06c67714-3386-4832-93da-5fb7b8ffceca",
-      name: "Ruta 2",
-      description: "De ejemplo",
-      author: "labra",
-      date: Date.now(),
-      points: [
-        { lat: -24.397, lng: 130.644, name: "Cascada", description: "Muy guapa" },
-        { lat: -25.297, lng: 129.644 },
-        { lat: -24.297, lng: 128.644 },
-        { lat: -23.397, lng: 127.644, name: "Lago" },
-        { lat: -24.197, lng: 126.644 }
-      ]
-    }
-  ];
+  const {webId} = props;
+  var friends = [];
+  const [routes, setRoutes] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const FileClient = require("solid-file-client");
+  const solidAuth = require("solid-auth-cli");
+  const fileClient = new FileClient(solidAuth)
+
+     useEffect(() => {
+        getAmigos();
+       getRoutes();
+     }, [])
+
+ const getAmigos = async () => {
+
+      
+
+        const doc =  await fetchDocument(webId);
+        const me = doc.getSubject(webId);
+        const myFriends = me.getAllRefs(foaf.knows);
+        friends = myFriends
+        friends.map(async friend =>{
+
+    
+          setIsLoading(true)
+        
+          const path = friend.replace('/profile/card#me','/public/routes');
+      
+          var folder = await fileClient.readFolder(path);
+      
+          Promise.all(folder.files.map(e => fileClient.readFile(e.url))).then(values => {
+            var routes = values.map(v => { try { return JSON.parse(v) } catch (err) { return undefined } }).filter(x => x)
+            setRoutes(routes);
+          }).finally(() => setIsLoading(false))
+
+        })
+      }
+
+const getRoutes = ()=>{
+  
+
+}      
+
 
   return (
-    <RouteMapPageContent {... { routes }} />
+    <RouteMapPageContent isLoading={isLoading} routes={routes} />
   );
 }

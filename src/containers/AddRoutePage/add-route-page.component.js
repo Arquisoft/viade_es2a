9967113d574d1d43857c2f61c76701b5382b4942
Nerea Valdés Egocheaga <nerevalego@gmail.com>
2020-03-14 22:40:34 +0100

    Pagina Nueva Ruta Finalizada

--- src/containers/AddRoutePage/add-route-page.component.js
+++ src/containers/AddRoutePage/add-route-page.component.js
@@ -1,28 +1,45 @@
-import React  from 'react';
+import React from 'react';
 import CamposRuta from './Hijos/Campos/camposNuevaRuta';
 import { AddRouteHolder, MapHolder } from './add-route-page.style';
-import Mapa from './Hijos/Mapa/mapa'
+import Map from './Hijos/Mapa/mapa'
 
+import { v4 as uuid } from 'uuid';
 
+import { storageHelper } from '@utils';
 
+const AddRoutePage = ({ webId }) => {
 
-const AddRoutePage = () => {
- //const puntos=[{ lat: -34.397, lng: 150.644 },{ lat: -35.397, lng: 150.644 }];
- //const googleMapURL = `https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}&v=3.exp&libraries=geometry,drawing,places`
- const googleMapURL = `https://maps.googleapis.com/maps/api/js?key=AIzaSyAOk4abibzy-RPB3hyqHrln6YfCEmOnJoo&v=3.exp&libraries=geometry,drawing,places`
- 
-return (
-  <AddRouteHolder>
-    <CamposRuta/>
-    <Mapa //{... {puntos}}
+  const googleMapURL = "https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}&v=3.exp&libraries=geometry,drawing,places"
+
+  const points = [];
+
+  const onPointAdded = point => {
+    points.push(point);
+  }
+
+  const onSave = ({ name, description }) => {
+    const route = {
+      id: uuid(),
+      name: name,
+      description: description,
+      date: Date.now(),
+      author: webId
+    }
+
+    storageHelper.saveRoute(webId, route);
+  }
+
+  return (
+    <AddRouteHolder>
+      <CamposRuta {...{ onSave }} />
+      <Map {...{ onPointAdded }}
         googleMapURL={googleMapURL}
-        loadingElement={<p>Cargando</p>}
+        loadingElement={<MapHolder />}
         containerElement={<MapHolder />}
         mapElement={<MapHolder />}
       />
-  </AddRouteHolder>
-        
-);
+    </AddRouteHolder>
+  );
 };
 
 export default AddRoutePage;

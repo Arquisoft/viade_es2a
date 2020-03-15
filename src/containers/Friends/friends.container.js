ad952c2e4196d83fe706517373a4faee207a7dc5
uo265363 <UO265363@uniovi.es>
2020-03-15 01:04:55 +0100

    Añadida animacion de carga a Mis Amigos

--- src/containers/Friends/friends.container.js
+++ src/containers/Friends/friends.container.js
@@ -1,44 +1,30 @@
-import React, { useState, useEffect } from 'react';
-// import {} from '@solid/query-ldflex';
-// import { namedNode } from '@rdfjs/data-model';
-import { FriendsPageContent } from './friends.component';
-// import { successToaster, errorToaster } from '@utils';
-import { foaf } from 'rdf-namespaces';
-import { fetchDocument } from 'tripledoc';
+import React, { useState, useEffect } from "react";
+import { FriendsPageContent } from "./friends.component";
+import { foaf } from "rdf-namespaces";
+import { fetchDocument } from "tripledoc";
 
-// const $rdf = require('rdflib');
-// const store = new $rdf.graph();
-// const fetcher = new $rdf.Fetcher(store);
-// const FOAF = $rdf.Namespace('http://xmlns.com/foaf/0.1/');
-// const data = require('@solid/query-ldflex');
 const FriendsContainer = props => {
-      
-  const {webId} = props;
-  const [friends, setFriends] = useState([])
+  const { webId } = props;
+  const [friends, setFriends] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-     useEffect(() => {
-       getAmigos()
-     }, [])
+  useEffect(() => {
+    getAmigos();
+  }, []);
 
- const   getAmigos = async () => {
-      
-        const doc =  await fetchDocument(webId);
-        const me = doc.getSubject(webId);
-        console.log(me)
-        const myFriends = me.getAllRefs(foaf.knows);
-        setFriends(myFriends);
-      }
-      
-        //const friendChosen = this.state;
-        //const amigos = this.getFriends();
-          
+  const getAmigos = async () => {
+    setIsLoading(true);
+    const doc = await fetchDocument(webId);
+    const me = doc.getSubject(webId);
+    console.log(me);
+    const myFriends = me.getAllRefs(foaf.knows);
+    setFriends(myFriends);
+    setIsLoading(false);
+  };
 
-          return (
-            <FriendsPageContent {...{ webId, friends,setFriends }} />
-          );
+  return (
+    <FriendsPageContent isLoading={isLoading} webId={webId} friends={friends} />
+  );
+};
 
-        
-
-}
-
-export default FriendsContainer
+export default FriendsContainer;

7d05b8ece1703ae093c2e23d2c045f267cee9c67
uo265363 <UO265363@uniovi.es>
2020-03-27 19:02:35 +0100

    Servicio de comentarios

--- src/services/index.js
+++ src/services/index.js
@@ -2,10 +2,12 @@ import Provider from './provider';
 import routeService from './route-service';
 import friendService from './friend-service';
 import groupService from './group-service';
+import commentService from './comment-service'
 
 export {
     Provider,
     routeService,
     friendService,
-    groupService
+    groupService,
+    commentService
 };

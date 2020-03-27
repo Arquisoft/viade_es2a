ae89c37d89395303fdfe9334beec91939eb4eb4b
Marcos <marcosav2010@gmail.com>
2020-03-27 19:56:23 +0100

    Created route creation base

--- src/components/RouteCreationPanel/route-creation-panel.component.js
+++ src/components/RouteCreationPanel/route-creation-panel.component.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { RouteFields, Map } from './children';
 import {
+  LeftPanel,
   CreationPanelHolder,
   MapHolder
 } from './route-creation-panel.style';
+
+import { successToaster } from '@utils';
+
+import { WaypointMenu } from './children';
 
 import { useTranslation } from 'react-i18next';
 
@@ -13,46 +18,92 @@ import { errorToaster } from '@utils';
 const RouteCreationPanel = ({ webId, onRouteCreation }) => {
 
   const { t } = useTranslation();
-  
+
   const googleMapURL = `https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}&v=3.exp&libraries=geometry,drawing,places`;
 
-  const points = [];
+  const [trackpoints, setTrackpoints] = useState([]);
+  const [waypoints, setWaypoints] = useState([]);
+  const [addingWaypoint, setAddingWaypoint] = useState(false);
 
-  const onPointAdded = point => {
-    points.push(point);
-  }
+  const onWaypointCreation = () => {
+    setAddingWaypoint(true);
+  };
+
+  const onPointAdd = point => {
+    if (addingWaypoint) {
+      setAddingWaypoint(false);
+      setWaypoints(waypoints.concat(point));
+    } else {
+      successToaster(t('route.edit.pointAdded'), t('route.edit.pointAddedTitle'));
+      setTrackpoints(trackpoints.concat(point));
+    }
+  };
+
+  const onPointDragged = (index, { lat, lng }, waypoint) => {
+    if (waypoint) {
+      waypoints[index].lat = lat;
+      waypoints[index].lng = lng;
+      setWaypoints([...waypoints]);
+    } else {
+      trackpoints[index].lat = lat;
+      trackpoints[index].lng = lng;
+      setTrackpoints([...trackpoints]);
+    }
+  };
+
+  const onWaypointDelete = index => {
+    waypoints.splice(index, 1);
+    setWaypoints([...waypoints]);
+  };
+
+  const onTrackpointDelete = index => {
+    trackpoints.splice(index, 1);
+    setTrackpoints([...trackpoints]);
+  };
 
   const onError = error => {
     errorToaster(error, 'Error');
-  }
+  };
 
   const onSave = async ({ name, description }) => {
-    if (!points.length) {
+    if (!trackpoints.length) {
       onError(t('route.edit.noPoints'));
       return;
     }
+
+    let outWaypoints = waypoints.map(({ lat, lng, name, desc }) => {
+      return { latitude: lat, longitude: lng, name, desc };
+    });
+
+    let points = trackpoints.map(({ lat, lng }) => {
+      return { latitude: lat, longitude: lng };
+    });
 
     const route = {
       name: name,
       description: description,
       date: Date.now(),
       author: webId,
-      points: points
-    }
+      waypoints: outWaypoints,
+      points
+    };
 
     await onRouteCreation(route);
-  }
+  };
 
   return (
     <CreationPanelHolder>
-      <RouteFields {...{ onSave, onError }} />
+      <LeftPanel>
+        <Map {...{ waypoints, trackpoints, onPointAdd, onPointDragged, onTrackpointDelete }}
+          googleMapURL={googleMapURL}
+          loadingElement={<MapHolder />}
+          containerElement={<MapHolder />}
+          mapElement={<MapHolder />}
+        />
+        <RouteFields {...{ onSave, onError }} />
+      </LeftPanel>
 
-      <Map {...{ onPointAdded }}
-        googleMapURL={googleMapURL}
-        loadingElement={<MapHolder />}
-        containerElement={<MapHolder />}
-        mapElement={<MapHolder />}
-      />
+      <WaypointMenu {...{ waypoints, onWaypointDelete, onWaypointCreation }} />
     </CreationPanelHolder>
   );
 };

a0ecf1a6c88ead3a81acb29eb3704b06f19f912b
Nerea Valdés Egocheaga <nerevalego@gmail.com>
2020-04-09 21:56:19 +0200

    First basic test route creation panel

--- src/components/RouteCreationPanel/route-creation-panel.test.js
+++ src/components/RouteCreationPanel/route-creation-panel.test.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import { render, cleanup } from 'react-testing-library';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { cleanup } from 'react-testing-library';
 import RouteCreationPanel from './route-creation-panel.component';
+import { shallow, mount, render } from 'enzyme';
+import  Enzyme from 'enzyme';
+import  Adapter  from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
 
 afterAll(cleanup);
 
-describe.only('RouteCreationPanel', () => {
-  const { container, rerender } = render(
-    <Router>
-      <RouteCreationPanel />
-    </Router>
-  );
+describe.only('Route Creation Panel tests', () => {
 
-  it('renders without crashing', () => {
+  const { container, rerender } = render(<RouteCreationPanel />);
+
+  it('should render without crashing', () => {
     expect(container).toBeTruthy();
   });
+
 });

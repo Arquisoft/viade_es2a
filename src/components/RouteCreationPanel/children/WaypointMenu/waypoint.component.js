3ee0b3011f201c11c6516b9fff5157f3082c24ef
Luis <uo264691@uniovi.es>
2020-04-09 14:49:22 +0200

    waypoint.test.js primeros test que funcionan bien

--- src/components/RouteCreationPanel/children/WaypointMenu/waypoint.component.js
+++ src/components/RouteCreationPanel/children/WaypointMenu/waypoint.component.js
@@ -9,6 +9,7 @@ const Waypoint = ({ index, waypoint, onWaypointDelete }) => {
         <div className = "waypoint">
             <div className = "waypoint_name">{waypoint.name ? waypoint.name : t("route.no_name")}</div>
             <div className = "waypoint_description">{waypoint.description ? waypoint.description : t("route.no_description")}</div>
+            <p className='pepe'>AAA</p>
             <button onClick={() => onWaypointDelete(index)}>Delete</button>
         </div>
     )
--- src/components/RouteCreationPanel/children/WaypointMenu/waypoint.test.js
+++ src/components/RouteCreationPanel/children/WaypointMenu/waypoint.test.js
@@ -1,6 +1,14 @@
 import React from 'react';
-import {render, cleanup } from 'react-testing-library';
+import '@testing-library/jest-dom/extend-expect';
+import { cleanup } from 'react-testing-library';
+import Enzyme,{render,mount,shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import Waypoint from './waypoint.component';
+
+Enzyme.configure({adapter: new Adapter()});
+
+
+
 
 afterAll(cleanup);
 
@@ -8,29 +16,35 @@ const waypoint = {
     lat: 24.3,
     lng: -2.2,
     name: 'Test',
-    description: 'Teste'
+    description: 'Test description'
 };
 
 describe.only('Waypoint', () => {
-    const { container } = render(
-        <Waypoint {...{ waypoint }} />
-    );
+    let wrapper;
+    beforeEach(()=>{
+        wrapper = mount( <Waypoint {...{ waypoint }} />);
+
+    })
+    
 
     it('renders without crashing', () => {
-        expect(container).toBeTruthy();
+        
+        console.log(wrapper.debug());
+        expect(wrapper).toBeTruthy();
     });
-});
+   
 
-it('render no waypoint when waypoint is empty', () => {
+    it('render waypoint correct name', () => {
     
-    const wrapper = render(<Waypoint />);
-    expect(wrapper.find("waypoint").length).toBe(0);
+    expect(wrapper.find('.waypoint_name').text()).toContain("Test");
+    });
+    
+    it('render waypoint correct description', () => {
+    
+        expect(wrapper.find('.waypoint_description').text()).toBe("Test description");
+        });
+    
 });
 
 
-it('render waypoint', () => {
-
-const wrapper = render(<Waypoint waypoint={waypoint} />);
-expect(wrapper.find("waypoint_name").text).toBe("Test");
-});
 

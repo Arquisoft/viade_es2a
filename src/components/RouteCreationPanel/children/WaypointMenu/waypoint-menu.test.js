679bdbcf64620f3a740ab81f6ff043ee1f8e5474
Luis <uo264691@uniovi.es>
2020-04-10 13:13:51 +0200

    waypoints-menu.test 1.0

--- src/components/RouteCreationPanel/children/WaypointMenu/waypoint-menu.test.js
+++ src/components/RouteCreationPanel/children/WaypointMenu/waypoint-menu.test.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import { render, cleanup } from 'react-testing-library';
+import {  cleanup } from 'react-testing-library';
 import WaypointMenu from './waypoint-menu.component';
+import Enzyme,{render,mount,shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({adapter: new Adapter()});
 
 afterAll(cleanup);
 
 const waypoints = [{
   lat: 24.3,
   lng: -2.2,
-  name: 'Test',
-  description: 'Teste'
+  name: 'Test1',
+  description: 'Teste1'
 }, {
   lat: 14.3,
   lng: -12.2,
@@ -16,12 +20,36 @@ const waypoints = [{
   description: 'Teste2'
 }];
 
+
+
 describe.only('WaypointMenu', () => {
-  const { container } = render(
-    <WaypointMenu {...{ waypoints }} />
-  );
+  let wrapper;
+  let wrapperVacio;
+  beforeEach(()=>{
+      wrapper = mount( <WaypointMenu {...{ waypoints }} />);
+      wrapperVacio = shallow( <WaypointMenu waypoints={[]} />);
+  })
 
   it('renders without crashing', () => {
-    expect(container).toBeTruthy();
+    expect(wrapper).toBeTruthy();
   });
+
+  it('render no waypoints', () => {
+    
+    expect(wrapperVacio.find('Waypoint')).toHaveLength(0);
+    });
+
+  it('render all waypoints', () => {
+    
+    expect(wrapper.find('Waypoint')).toHaveLength(waypoints.length);
+    });
+    
+  it('render all waypoints correctly', () => {
+    console.log(wrapper.debug());
+  expect(wrapper.find('Waypoint')).toHaveLength(waypoints.length);
+  
+ 
 });
+});
+
+

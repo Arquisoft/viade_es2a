5d28b2565b3450b60c64c00fe64002c7520dbf72
Luis <uo264691@uniovi.es>
2020-04-12 14:34:23 +0200

    friends-holder.test "renders on creation"

--- src/containers/Feed/children/FeedSidePanel/children/FriendHolder/friend-holder.test.js
+++ src/containers/Feed/children/FeedSidePanel/children/FriendHolder/friend-holder.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, cleanup } from 'react-testing-library';
+import {  cleanup } from 'react-testing-library';
 import FriendHolder from './friend-holder.component';
 import { RouteMapContext } from '@containers/MyRoutes/my-routes.component';
 
@@ -7,20 +7,38 @@ import { FeedContext } from '../../../../feed.component';
 
 import 'jest-dom/extend-expect';
 
+import Enzyme,{mount} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({adapter: new Adapter()});
+
 const webId = "https://marcosav.inrupt.net/profile/card#me";
 
 describe('FriendHolder', () => {
     afterAll(cleanup);
 
-    const { container } = render(
+    let wrapper;
+    beforeEach(()=>{
+        wrapper = mount(
         <RouteMapContext.Provider>
             <FeedContext.Provider value={{ isDeletedFriend: f => false, isSelectedFriend: f => false }}>
                 <FriendHolder {...{ webId }} />
             </FeedContext.Provider>
-        </RouteMapContext.Provider>
-    );
-
-    test('renders without crashing', () => {
-        expect(container).toBeTruthy();
+        </RouteMapContext.Provider>);
     });
+
+    it('renders without crashing', () => {
+        expect(wrapper).toBeTruthy();
+    });
+
+    it('renders on creation', () => {
+    
+        expect(wrapper.find('.FriendHolderWrapper')).toBeDefined();
+        expect(wrapper.find('.FriendHolderHeader')).toBeDefined();
+        expect(wrapper.find('.FriendButtonContainer')).toBeDefined();
+        expect(wrapper.find('.RouteContainer')).toBeDefined();
+        expect(wrapper.find('.no-routes')).toBeDefined();
+        expect(wrapper.find('.loading')).toBeDefined();
+    });
+    
 });

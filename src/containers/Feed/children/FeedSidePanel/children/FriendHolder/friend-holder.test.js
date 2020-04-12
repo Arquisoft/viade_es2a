d61b5582b0f22ec5f29ef57838879b51655968f6
Marcos <marcosav2010@gmail.com>
2020-04-12 19:45:37 +0200

    Fixed code format

--- src/containers/Feed/children/FeedSidePanel/children/FriendHolder/friend-holder.test.js
+++ src/containers/Feed/children/FeedSidePanel/children/FriendHolder/friend-holder.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {  cleanup } from 'react-testing-library';
+import { cleanup } from 'react-testing-library';
 import FriendHolder from './friend-holder.component';
 import { RouteMapContext } from '@containers/MyRoutes/my-routes.component';
 
@@ -7,10 +7,10 @@ import { FeedContext } from '../../../../feed.component';
 
 import 'jest-dom/extend-expect';
 
-import Enzyme,{mount} from 'enzyme';
+import Enzyme, { mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
-Enzyme.configure({adapter: new Adapter()});
+Enzyme.configure({ adapter: new Adapter() });
 
 const webId = "https://marcosav.inrupt.net/profile/card#me";
 
@@ -18,13 +18,13 @@ describe('FriendHolder', () => {
     afterAll(cleanup);
 
     let wrapper;
-    beforeEach(()=>{
+    beforeEach(() => {
         wrapper = mount(
-        <RouteMapContext.Provider>
-            <FeedContext.Provider value={{ isDeletedFriend: f => false, isSelectedFriend: f => false }}>
-                <FriendHolder {...{ webId }} />
-            </FeedContext.Provider>
-        </RouteMapContext.Provider>);
+            <RouteMapContext.Provider>
+                <FeedContext.Provider value={{ isDeletedFriend: f => false, isSelectedFriend: f => false }}>
+                    <FriendHolder {...{ webId }} />
+                </FeedContext.Provider>
+            </RouteMapContext.Provider>);
     });
 
     it('renders without crashing', () => {
@@ -32,7 +32,6 @@ describe('FriendHolder', () => {
     });
 
     it('renders on creation', () => {
-    
         expect(wrapper.find('.FriendHolderWrapper')).toBeDefined();
         expect(wrapper.find('.FriendHolderHeader')).toBeDefined();
         expect(wrapper.find('.FriendButtonContainer')).toBeDefined();
@@ -42,6 +41,4 @@ describe('FriendHolder', () => {
 
         console.log(wrapper.debug());
     });
-
-    
 });

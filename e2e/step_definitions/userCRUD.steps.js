ba89343f69f58465f4ae2c347926474093a9c8ef
Jesus <uo252137@uniovi.es>
2020-04-21 06:02:40 +0200

    Add friend test

---
+++ e2e/step_definitions/userCRUD.steps.js
@@ -0,0 +1,122 @@
+const expect = require('expect-puppeteer');
+const puppeteer = require('puppeteer');
+const { defineFeature, loadFeature } = require('jest-cucumber');
+
+const feature1 = loadFeature('./e2e/features/userCRUD/addFriend.feature');
+
+let port = 3000;
+let url = 'http://localhost:' + port;
+
+let webId1 = "https://viadees2atester1.inrupt.net/profile/card#me";
+let username1 = "viadeES2Atester1";
+let password1 = "viadeES2A_password_1";
+
+let webId2 = "https://viadees2atester2.inrupt.net/profile/card#me";
+let webId3 = "https://alejandrine3.inrupt.net/profile/card#me";
+let webId4 = "https://jesusperez97.inrupt.net/profile/card#me";
+
+var page;
+
+function delay(time) {
+    return new Promise(function (resolve) {
+        setTimeout(resolve, time)
+    });
+}
+
+beforeAll(async () => {
+    //Open browser
+    const browser = await puppeteer.launch({
+        //headless let watch the chrome window interacting with the application
+        headless: false,
+        defaultViewport: null
+    });
+    page = await browser.newPage();
+    await page.goto(url);
+
+    /** 
+     *  ######################################################################################
+     *  ################# Log in #############################################################
+     *  ######################################################################################
+     */
+
+    //Wait for login button
+    await page.waitForSelector('button[data-testid="provider-form-button"]');
+    //Check if already logged
+    const logueate = await expect(page).toMatchElement('button[data-testid="provider-form-button"]');
+    //If not already logged
+    if (logueate !== null) {
+        //Fill webId
+        await expect(page).toFill('input[name="idp"]', webId1);
+        await page.click('[type="submit"]');
+
+        //Wait for user-pass screen
+        await page.waitForSelector('input[name="username"]');
+        //Fill user and pass
+        await expect(page).toFill('input[name="username"]', username1);
+        await expect(page).toFill('input[name="password"]', password1);
+        await page.click('[id="login"]');
+
+        //Wait for the main nav-bar icon
+        await page.waitForSelector('img[src="img/bars-nav.svg"]');
+    } else {
+        //Wait for the main nav-bar icon
+        await page.waitForSelector('img[src="img/bars-nav.svg"]');
+    }
+
+});
+
+
+defineFeature(feature1, test1 => {
+    test1('Paco wants to add new friends', ({ given, when, then }) => {
+
+        given('Paco has logged in successfully into the application', () => {
+            //Already done in beforeAll() statement
+        });
+
+        when('Paco adds three new friends by introducing their webIds', async () => {
+            await page.goto("http://localhost:" + port + "/#/feed");
+
+            // Add Pedro
+            await page.waitForSelector('button[name="create-route-floating-button"]');
+            await page.click('button[name="create-route-floating-button"]');
+
+            await page.waitForSelector('input[name="value-friend-webID"]');
+            await expect(page).toFill('input[name="value-friend-webID"]', webId2);
+
+            await page.click('button[name="add-friend-button"]');
+
+            await delay(5000);
+
+            // Add Alejandro
+            await page.waitForSelector('button[name="create-route-floating-button"]');
+            await page.click('button[name="create-route-floating-button"]');
+
+            await page.waitForSelector('input[name="value-friend-webID"]');
+            await expect(page).toFill('input[name="value-friend-webID"]', webId3);
+
+            await page.click('button[name="add-friend-button"]');
+
+            await delay(5000);
+
+            // Add Jesus
+            await page.waitForSelector('button[name="create-route-floating-button"]');
+            await page.click('button[name="create-route-floating-button"]');
+
+            await page.waitForSelector('input[name="value-friend-webID"]');
+            await expect(page).toFill('input[name="value-friend-webID"]', webId4);
+
+            await page.click('button[name="add-friend-button"]');
+
+            await delay(5000);
+        });
+
+        then('Paco can view his new friends in his friends list', async () => {
+            // Check new friends added
+            await page.waitForFunction('document.querySelector("body").innerText.includes("viadees2atester2.inrupt.net")');
+            await page.waitForFunction('document.querySelector("body").innerText.includes("alejandrine3.inrupt.net")');
+            await page.waitForFunction('document.querySelector("body").innerText.includes("jesusperez97.inrupt.net")');
+        });
+
+    });
+
+});

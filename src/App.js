2f45905f3007271b29d424a927277642646e8892
Marcos <marcosav2010@gmail.com>
2020-04-30 15:57:53 +0200

    Added download feature to routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React, { Fragment, Suspense } from 'react';
 import { toast, Slide } from 'react-toastify';
 import { Loader } from '@util-components';
-import { ThemeProvider } from 'styled-components';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import Routes from './routes';
-import theme from './utils/theme';
 import 'react-toastify/dist/ReactToastify.css';
 import 'flag-icon-css/css/flag-icon.min.css';
 import 'normalize.css';
@@ -18,25 +16,23 @@ library.add(fas);
 library.add(faGithub);
 const App = () => (
   <Suspense fallback={<Loader />}>
-    <ThemeProvider theme={theme}>
-      <Fragment>
-        <Routes />
-        <Toaster
-          {...{
-            autoClose: 5000,
-            position: toast.POSITION.TOP_CENTER,
-            newestOnTop: true,
-            closeOnClick: true,
-            pauseOnVisibilityChange: true,
-            draggable: true,
-            className: 'solid-toaster-container',
-            toastClassName: 'solid-toaster',
-            bodyClassName: 'solid-toaster-body',
-            transition: Slide
-          }}
-        />
-      </Fragment>
-    </ThemeProvider>
+    <Fragment>
+      <Routes />
+      <Toaster
+        {...{
+          autoClose: 5000,
+          position: toast.POSITION.TOP_CENTER,
+          newestOnTop: true,
+          closeOnClick: true,
+          pauseOnVisibilityChange: true,
+          draggable: true,
+          className: 'solid-toaster-container',
+          toastClassName: 'solid-toaster',
+          bodyClassName: 'solid-toaster-body',
+          transition: Slide
+        }}
+      />
+    </Fragment>
   </Suspense>
 );
 
